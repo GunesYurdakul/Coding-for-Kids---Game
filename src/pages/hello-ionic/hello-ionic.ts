46b84ed15338a0a2b62e6dabc5a554a09126c92a
gunesyurdakul <gunesyurdakul@gmail.com>
2017-12-01 19:00:00 +0300

    co

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -7,31 +7,41 @@ import { Component } from '@angular/core';
 export class HelloIonicPage {
 
   tes = "can";
+  list = [[0,1]]
   constructor() {
-
   }
-
   left() {
     this.tes = "left"
+    this.list.push([0,-1]);
+    
   }
 
   right() {
     this.tes = "right"
+    this.list.push([0,1]);
+    
   }
 
   up() {
     this.tes = "up"
+    this.list.push([1,0]);
+    
   }
 
   down() {
     this.tes = "down"
+    this.list.push([-1,0]);
+    
   }
 
   function() {
     this.tes = "function"
+    
   }
 
   unlock() {
     this.tes = "unlock"
+    this.list.push([2,2]);
+    
   }
 }
