7ed4eedaec87b3189d2686f3bea5a48b377c4f38
gunesyurdakul <gunesyurdakul@gmail.com>
2017-12-01 21:01:27 +0300

    button

---
+++ src/pages/Command.ts
@@ -0,0 +1,10 @@
+class Command{
+    x = 0
+    y = 0
+    name=""
+    constructor(y,x,name_in) {
+      this.x=x
+      this.y=y
+      this.name=name_in
+    }
+  }
--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -4,44 +4,113 @@ import { Component } from '@angular/core';
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
+
 export class HelloIonicPage {
 
   tes = "can";
   list = [[0,1]]
+  command_names=[]
+  function_list=[]
+  function_command_names=[]  
+  is_function=0
   constructor() {
   }
   left() {
-    this.tes = "left"
-    this.list.push([0,-1]);
-    
+    if(this.is_function){
+      this.tes = "left"
+      this.function_list.push([0,-1]);
+      this.function_command_names.push("arrow-back");
+    }
+    else{
+      this.tes = "left"
+      this.list.push([0,-1]);
+      this.command_names.push("arrow-back");
+    }
   }
 
   right() {
-    this.tes = "right"
-    this.list.push([0,1]);
-    
+    if(this.is_function){
+      this.tes = "right"
+      this.function_list.push([0,1]);
+      this.function_command_names.push("arrow-forward");
+    }
+    else{
+      this.tes = "right"
+      this.list.push([0,1]);
+      this.command_names.push("arrow-forward");
+    }
   }
 
   up() {
-    this.tes = "up"
-    this.list.push([1,0]);
+
+    if(this.is_function){
+      this.tes = "up"
+      this.function_list.push([1,0]);
+      this.function_command_names.push("arrow-up");
+    }
+    else{
+      this.tes = "up"
+      this.list.push([1,0]);
+      this.command_names.push("arrow-up");
+    }
     
   }
 
   down() {
-    this.tes = "down"
-    this.list.push([-1,0]);
+
+    if(this.is_function){
+      this.tes = "down"
+      this.function_list.push([1,0]);
+      this.function_command_names.push("arrow-down");
+    }
+    else{
+      this.tes = "down"
+      this.list.push([-1,0]);
+      this.command_names.push("arrow-down");  
+    }
+    
     
   }
 
   function() {
-    this.tes = "function"
-    
+    if(this.is_function){
+      this.tes = "function"
+      this.function_list.push([3,3]);
+      this.function_command_names.push("albums");
+    }
+    else{
+      this.tes = "function"
+      this.list.push([3,3]);
+      this.command_names.push("albums");
+    }
+
+    for(let entry of this.list)
+    {
+      this.tes+=" "
+      this.tes += entry
+    }
   }
 
   unlock() {
-    this.tes = "unlock"
-    this.list.push([2,2]);
+   
+    if(this.is_function){
+      this.tes = "unlock"
+      this.function_list.push([2,2]);
+      this.function_command_names.push("unlock");
+    }
+    else{
+      this.tes = "unlock"
+      this.list.push([2,2]);
+      this.command_names.push("unlock");
+    }
     
+  }
+
+  set_function(){
+    this.is_function=1    
+  }
+
+  reset_function(){
+    this.is_function=0   
   }
 }
